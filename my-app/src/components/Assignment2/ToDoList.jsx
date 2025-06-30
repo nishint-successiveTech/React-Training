56b8b787f4fed09f292ce03dadfcb2e888942bf6
Nishint Goyal <nishint.goyal@nishintgoyalvrs-17.successivetech.com>
2025-06-30 15:20:32 +0530

    Added Q3

---
+++ my-app/src/components/Assignment2/ToDoList.jsx
@@ -0,0 +1,57 @@
+"use client"
+
+import React, { useState } from 'react';
+
+const ToDoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim() === '') return;
+    setTodos([...todos, { id: Date.now(), text: input, completed: false }]);
+    setInput('');
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h2>To-Do List</h2>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter a new task"
+      />
+      <button onClick={addTodo}>Add</button>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodo(todo.id)}
+            />
+            <span>
+              {todo.completed ? <s>{todo.text}</s> : todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDoList;
