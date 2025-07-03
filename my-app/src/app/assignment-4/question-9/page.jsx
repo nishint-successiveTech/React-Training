bedd0d8baa052f17483b45537f7f822ad39d6a5a
Nishint Goyal <nishint.goyal@nishintgoyalvrs-17.successivetech.com>
2025-07-03 15:22:14 +0530

    Updated Assignment-9

---
+++ my-app/src/app/assignment-4/question-9/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <center>
+        <h1>IMPLEMENTED MUI IN THE ASSIGNMENT</h1>
+      </center>
+    </div>
+  );
+};
+
+export default page;
